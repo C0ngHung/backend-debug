6a4f6c4b07ff7acc39ab9487136ba72a074d5f6d
Caelus <C0ngHungDCH@gmail.com>
2026-04-24 21:58:01 +0700

    init project

---
+++ src/main/java/smartosc/conghung/BackendDebugApplication.java
@@ -0,0 +1,13 @@
+package smartosc.conghung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDebugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDebugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/smartosc/conghung/BackendDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package smartosc.conghung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
